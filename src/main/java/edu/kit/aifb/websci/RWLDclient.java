8810f7c1ec0a35133813d20574e6bfaf56abe03c
Tobias Käfer <6708974+kaefer3000@users.noreply.github.com>
2019-01-31 23:52:23 +0100

    WebID+TLS example

--- src/main/java/edu/kit/aifb/websci/RWLDclient.java
+++ src/main/java/edu/kit/aifb/websci/RWLDclient.java
@@ -12,9 +12,19 @@ import org.semanticweb.yars.nx.Nodes;
 public class RWLDclient {
 
 	public static void main(String[] args) {
+		testHttp();
+		testHttpsWebIdTLS();
+	}
+		
+	public static void testHttp() {
 
-		Client client = ClientBuilder.newClient();
-		Response res = client.target(
+		System.err.println("Testing HTTP");
+
+		Client client;
+		Response res;
+
+		client = ClientBuilder.newClient();
+		res = client.target(
 				"http://www.w3.org/1999/02/22-rdf-syntax-ns"
 				)
 				.request(
@@ -29,6 +39,56 @@ public class RWLDclient {
 			}
 		else
 			System.err.println("Request was not successful.");
+	}
+
+	/**
+	 * You need a client certificate and supply a corresponding {@link SSLContext} to the {@link ClientBuilder}.
+	 * You can use Jersey's SslConfigurator to generate the {@link SSLContext} like in:
+	 * 
+	 * <br><br>
+	 * <code>
+	 * 	org.glassfish.jersey.SslConfigurator sslconfig = SslConfigurator.newInstance(true);
+	 *  sslconfig.keyStoreFile("/path/to/keystore.p12");
+	 *  sslconfig.keyStoreType("PKCS12");
+	 *  sslconfig.keyStorePassword("t0p_s3cr3t");
+	 *  SSLContext sslcontext = sslconfig.createSSLContext();
+	 *  
+	 *  ClientBuilder.newBuilder().sslContext(sslcontext).build()
+	 * </code>
+	 * <br><br>
+	 * 
+	 * Or set the system variables:
+	 * <ul>
+	 * <li><pre>javax.net.ssl.keyStore</pre></li> 
+	 * <li><pre>javax.net.ssl.keyStorePassword</pre></li>
+	 * <li><pre>javax.net.ssl.keyStoreType</pre></li>
+	 * </ul>
+	 */
+	public static void testHttpsWebIdTLS() {
+
+		System.err.println("Testing HTTPS+WebID+TLS");
+
+		Client client;
+		Response res;
+
+		client = ClientBuilder.newBuilder().build();
+		
+		res = client.target(
+				"https://kaefer3000.databox.me/Inbox/"
+				)
+				.request(
+						MediaType.valueOf("text/turtle")
+						).get();
+
+		if (res.getStatus() >= 200 && res.getStatus() < 300)
+			// Successful request
+			for (Node[] nx : res.readEntity(new GenericType<Iterable<Node[]>>() {
+			})) {
+				System.out.println(Nodes.toString(nx));
+			}
+		else
+			System.err.println("Request was not successful." + res);
+		
 	}
 
 }
